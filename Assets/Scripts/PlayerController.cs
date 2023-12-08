882b7fd673d636b0aa1d7071b0ce52a0f950dd56
EyeCrown <theocarrasco2001@gmail.com>
2023-12-08 11:18:26 +0100

    Connect Unity with Visual Studio (Why Unity ?)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 movements;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    void Move(InputAction.CallbackContext context)
+    {
+        movements = context.
+    }
+
+    void Pass(InputAction.CallbackContext context)
+    {
+        Debug.Log("Pass: Do something");
+    }
+
+    void Shoot(InputAction.CallbackContext context)
+    {
+        Debug.Log("Shoot: Do something");
+    }
+}
