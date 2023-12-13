17ed922874035a5bcd36d7167ab0e03a5250b12a
PoulpeGracieux <124520945+PoulpeGracieux@users.noreply.github.com>
2023-12-13 14:41:57 +0100

    addVFXexplosionAndPassThrowLuciole

--- Assets/VFX/Ball/States.cs
+++ Assets/VFX/Ball/States.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 public class States : MonoBehaviour
 {
     [SerializeField] int etat = 0;
+    [SerializeField] SpriteRenderer render_1;
+    [SerializeField] SpriteRenderer render_2;
+    [SerializeField] TrailRenderer trail;
+    [SerializeField] ParticleSystem particle;
 
-
+    [SerializeField] Color color_1;
+    [SerializeField] Color color_2;
+    [SerializeField] Color color_3;
+    [SerializeField] Color color_4;
+    [SerializeField] Color color_5;
+    [SerializeField] Color color_6;
  
 
     // Start is called before the first frame update
@@ -20,17 +29,52 @@ public class States : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (etat > 2)
+        //switch (etat)
+        //{
+        //    case 0:
+        //        render_1;
+        //        break;
+        //    default:
+        //        break;
+        //}
+        if (etat == 0)
         {
-            
-            Debug.Log("sup 2");
-
+            ChangeColor(color_1);
+         
         }
 
-        if (etat < 2)
+        else if (etat == 1)
         {
 
-            Debug.Log("inf 2");
+            ChangeColor(color_2);
         }
+        else if (etat == 2)
+        {
+
+            ChangeColor(color_3);
+        }
+        else if (etat == 3)
+        {
+
+            ChangeColor(color_4);
+        }
+        else if (etat == 4)
+        {
+
+            ChangeColor(color_5);
+        }
+        else if (etat == 5)
+        {
+
+            ChangeColor(color_6);
+        }
+       
+    }
+    void ChangeColor(Color color)
+    {
+        render_1.material.SetColor("_EmisColor", color);
+        render_2.material.SetColor("_EmisColor", color);
+        trail.startColor = color;
+        particle.startColor = color;
     }
 }
